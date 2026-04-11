0124e887decb18fe15d338d5a0609255a8af44f5
omarmahany35-png <omarmahany35@gmail.com>
2026-04-11 16:17:46 +0200

    continue working on world building with starting on enemies creation.

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -7,5 +7,5 @@ func _on_body_entered(body):
 	timer.start()
 
 
-func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
