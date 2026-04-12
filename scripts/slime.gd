e53bd9044fd5a4b4ea9cbd8fa846418e607469b0
omarmahany35-png <omarmahany35@gmail.com>
2026-04-12 16:58:50 +0200

    Now I'm developing how enemies are moving

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
+const SPEED = 60
+  
+var direction = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+	if ray_cast_left.is_colliding():
+		direction = 1
+	position.x += direction * SPEED * delta
