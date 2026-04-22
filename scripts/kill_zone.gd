ca4e0d875be7a1d6a9e4ceb348f5446f220c981c
omarmahany35-png <omarmahany35@gmail.com>
2026-04-22 18:15:31 +0200

    made a cool dying and fast come back

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -4,8 +4,10 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("you died!")
+	Engine.time_scale = 0.5
 	timer.start()
 
 
 func _on_timer_timeout():
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
